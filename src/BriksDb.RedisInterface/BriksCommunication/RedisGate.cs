17c4f51b152f4627cf29fa2992b5d8cc6ff090d5
Waster <nikmenshov@mail.ru>
2015-05-05 15:33:46 +0300

    Squashed 'subtree/bricksdb/' changes from d491de8..e89ba2c
    e89ba2c add performance log to console
    0599539 fixes for redis interface
    b7da2d2 added get operation processing. have error with distributor system
    e32f359 added operation performers with efficiency counters
    b7043a5 added processing of messages from redis and interaction with briksDB
    b1a4c1c added layer between redis listener and briks proxy
    ff48432 changed name of project and added proxyAPI
    df67305 Redis server added. Can listen, accept SET messages from redis-benchmark, answer OK.
    
    git-subtree-dir: subtree/bricksdb
    git-subtree-split: e89ba2c9f5568495cfb00d1d21299f191d28ecbf

---
+++ src/BriksDb.RedisInterface/BriksCommunication/RedisGate.cs
@@ -0,0 +1,32 @@
+﻿using Qoollo.Client.Configuration;
+using Qoollo.Client.ProxyGate;
+
+namespace BricksDb.RedisInterface.BriksCommunication
+{
+    class RedisGate: ProxyApi
+    {
+        private IStorage<string, string> _data;
+
+    public RedisGate(NetConfiguration netConfiguration, ProxyConfiguration proxyConfiguration,
+        CommonConfiguration commonConfiguration, TimeoutConfiguration timeoutConfiguration)
+        : base(netConfiguration, proxyConfiguration, commonConfiguration, timeoutConfiguration)
+    {
+    }
+
+    public RedisGate(NetConfiguration netConfiguration, ProxyConfiguration proxyConfiguration,
+        CommonConfiguration commonConfiguration)
+        : base(netConfiguration, proxyConfiguration, commonConfiguration)
+    {
+    }
+
+    public IStorage<string, string> RedisTable
+    {
+        get { return _data; }
+    }
+
+    protected override void InnerBuild()
+    {
+        _data = RegistrateApi("RedisTable", false, new RedisDataProvider());            
+    }
+    }
+}
